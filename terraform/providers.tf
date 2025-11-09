2b12ebe8f6d8c52a8cdecb8e7585a27e15499fb4
mengweilin985 <61502637+mengweilin985@users.noreply.github.com>
2025-11-09 18:20:36 +0800

    f2

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,18 +7,7 @@ terraform {
   }
 }
 
-variable "project_id" {
-  type    = string
-  default = "github-action-cve-1"
-}
-
-variable "region" {
-  type    = string
-  default = "asia-southeast1"
-}
-
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file("key.json") # ← for local use only, not needed in GitHub Actions
 }
