b87a0182d090f256ac5808f070add78bbd190a1e
amretya <leeamrey@gmail.com>
2026-01-29 15:07:24 +0800

    Update test_gcr.py

--- test/test_gcr.py
+++ test/test_gcr.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("https://terraform-test-660449041854.asia-southeast1.run.app", files={'file': open('1.jpg', 'rb')})
+resp = requests.post("https://terraform-test-972205208187.asia-southeast1.run.app", files={'file': open('1.jpg', 'rb')})
 
 print(resp.json())
